4d4995b64f10b92698f9049e6a61c45bb2bc007e
jakeoetinger <jakeoetinger@gmail.com>
2020-04-26 13:26:30 -0700

    Added outliers. Changed layout.

--- code/DelayPlotMap.R
+++ code/DelayPlotMap.R
@@ -3,7 +3,6 @@ library(ggplot2)
 library(plyr)
 library(airportr)
 library(hflights)
-library(mapproj)
 
 #Get data for base map
 US <- map_data("state")
@@ -58,10 +57,19 @@ plot_df <- inner_join(by_dest_sum, US_airports, by = "dest") %>%
 #Filter for destinations with more than 100 flights
 plot_df <- plot_df %>% filter(nflights > 100)
 
+plot_df$mean_delay[(plot_df$mean_delay > 29 | plot_df$mean_delay < 0)] <- NA
+
 #Add a factor column for bins of mean_delay, if you want to map color 
 #to a categorical variable
 plot_df <- plot_df %>%
-  mutate(mean_delay_f = as_factor(round_any(plot_df$mean_delay,10,floor)))
+  mutate(mean_delay_f = as_factor(round_any(plot_df$mean_delay,5,floor)))
+
+plot_df$mean_delay_f2 <- cut(plot_df$mean_delay, breaks = seq(0,20, length.out = 4))
+
+text_df <- data.frame(x = c(-80,-95), y = c(43,40),
+                      origin = factor(c("HOU","HOU"), levels = c("HOU","IAH")),
+                      label = c("Flights to Newark (EWR)\nand Philadelphia (PHL)\nwere delayed for 30+\nminutes, on average.",
+                                "Over 110 trips, flights to\nBranson Airport (BKG)\ndeparted 3 minutes early,\non average."))
 
 #Plot the map using geom_polygon
 ggplot() + geom_polygon(data = US, aes(x = long, y = lat, group = group), 
@@ -69,7 +77,8 @@ ggplot() + geom_polygon(data = US, aes(x = long, y = lat, group = group),
   #Mark the airports with points
   geom_point(data = plot_df, 
              aes(x = lon, y = lat, 
-                 color = mean_delay), 
+                 color = mean_delay,
+                 fill = ""), 
              size = 0.1) +
   #Mark the flight paths with neat looking curves
   geom_curve(data = plot_df, 
@@ -82,22 +91,27 @@ ggplot() + geom_polygon(data = US, aes(x = long, y = lat, group = group),
        subtitle = "Departures from HOU vs. IAH",
        x = NULL, y = NULL) +
   #Set color palette for continuous scale
-  scale_color_viridis_c(option = "plasma",
-                        breaks = c(min(plot_df$mean_delay), max(plot_df$mean_delay)),
-                        labels = c("3 minutes early","30 minutes late"),
-                        guide = guide_colorbar(ticks = F)) +
+  scale_color_viridis_c(NULL, option = "plasma",
+                        breaks = c(min(plot_df$mean_delay, na.rm = T) + 2, 
+                                   max(plot_df$mean_delay, na.rm = T)) -1.5,
+                        labels = c("On time","20 minutes\nlate"),
+                        aesthetics = "color",
+                        guide = guide_colorbar(ticks = F),
+                        na.value = "white") +
+  scale_fill_manual(values = NA, name = NULL, guide = "none", labels = "Outlier") +
+  guides(fill=guide_legend(override.aes=list(shape=NA))) +
   #A whole bunch of tweaks to the theme
   theme(panel.background = element_rect(color = "#c2c2c2", fill = "#121212"),
         plot.background = element_rect(color = "#121212", fill = "#121212"),
-        plot.margin = margin(1,1,1,1,"cm"),
+        plot.margin = margin(0.5,0.5,0.5,0.5,"cm"),
         panel.grid = element_blank(),
         axis.ticks = element_blank(),
         axis.text = element_blank(),
-        legend.key.height = unit(.4,"cm"),
-        legend.key.width = unit(1,"cm"),
-        legend.position = c(0.82,1.23),
-        legend.direction = "horizontal",
-        legend.title = element_blank(),
+        # legend.key.height = unit(.4,"cm"),
+        # legend.key.width = unit(1,"cm"),
+        # legend.position = c(0.82,1.23),
+        # legend.direction = "horizontal",
+        # legend.title = element_blank(),
         legend.background = element_rect(fill = "#121212", color = "#121212"),
         text = element_text(color = "#c2c2c2"),
         #strip. arguments alter the facet labels
@@ -105,11 +119,13 @@ ggplot() + geom_polygon(data = US, aes(x = long, y = lat, group = group),
         strip.text = element_text(color = "#c2c2c2")) +
   #See note below:
   coord_quickmap() +
-  facet_wrap(~origin,
+  facet_grid(vars(origin), switch = "y",
              labeller = labeller(origin = c("HOU" = "Departing from HOU",
-                                            "IAH" = "Departing from IAH")))
+                                            "IAH" = "Departing from IAH"))) +
+  geom_text(data = text_df, aes(x = x, y = y, label = label),
+            size = 2.1, color = "white")
 
-ggsave("FlightMap2.png", height = 4, width = 10, bg = "#121212")
+ggsave("FlightMap3.png", height = 6, width = 7, bg = "#121212")
 
 #Note:
 #This sets the coordinate projection for the plot. coord_quickmap is not very 
