20a1a20781ff4f4c66b7c81620a00829fd164d6b
tburlingame <burlingt@oregonstate.edu>
2020-05-03 10:54:26 -0700

    Update volcano_heatmap_teresa.R
    added countours, updated column names

--- code/volcano_heatmap_teresa.R
+++ code/volcano_heatmap_teresa.R
@@ -1,14 +1,20 @@
+library(ggplot2)
+library(tidyverse)
 volcano_tbl <- as_tibble(volcano)
 colnames(volcano_tbl) <- 1:ncol(volcano) 
 volcano_tbl$row <- 1:nrow(volcano_tbl)
 
 heat_map <- volcano_tbl %>% pivot_longer(-contains("row"), names_to = "y", 
                                        values_to = "elevation")
-
+names(heat_map) <- c("x", "y", "elevation")
 heat_map$y <- as.integer(heat_map$y)
 ggplot(heat_map)+
-  geom_tile(aes(row,y, fill = elevation))+
-  scale_fill_distiller(palette = "Spectral")
+  geom_tile(aes(x,y, fill = elevation))+
+  scale_fill_distiller(palette = "Spectral")+
+  coord_equal() +
+  geom_contour(aes(x = x, y = y, z=elevation)) +
+  theme_bw()
+  
   
 filled.contour(volcano, color.palette = terrain.colors, asp = 1)
 
